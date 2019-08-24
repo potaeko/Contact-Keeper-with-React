7c06a165f090931c8719e098fd299d376fab5659
Thomas Ko <potaeko@hotmail.com>
2019-08-23 20:56:47 -0700

    ignore config file

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ if(process.env.NODE_ENV === 'production'){
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); //open postman and try GET http://localhost:5000, cannot get /
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
